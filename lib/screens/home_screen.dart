0b880043f9818d47f788bbce8f56491be8901c00
SuhaibRumi <84827527+SuhaibRumi@users.noreply.github.com>
2022-08-25 14:51:52 +0500

    day 1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,43 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+List<String> productName = [
+  'Mango',
+  'Orange',
+  'Grapes',
+  'Banana',
+  'Chery',
+  'Peach',
+  'Mixed Fruit Basket',
+];
+List<String> productUnit = [
+  'KG',
+  'Dozen',
+  'KG',
+  'Dozen',
+  'KG',
+  'KG',
+  'KG',
+];
+List<int> productPrice = [10, 20, 30, 40, 50, 60, 70];
+List<String> productImage = [
+  'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg',
+  'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg',
+  'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg',
+  'https://media.istockphoto.com/photos/banana-picture-id1184345169?s=612x612',
+  'https://media.istockphoto.com/photos/cherry-trio-with-stem-and-leaf-picture-id157428769?s=612x612',
+  'https://media.istockphoto.com/photos/single-whole-peach-fruit-with-leaf-and-slice-isolated-on-white-picture-id1151868959?s=612x612',
+  'https://media.istockphoto.com/photos/fruit-background-picture-id529664572?s=612x612',
+];
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +61,90 @@ class HomeScreen extends StatelessWidget {
           const SizedBox(
             width: 20,
           )
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: productName.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Image(
+                                height: 100,
+                                width: 100,
+                                image: NetworkImage(
+                                    productImage[index].toString()),
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      productName[index].toString(),
+                                      style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      productUnit[index].toString() +
+                                          r" $" +
+                                          productPrice[index].toString(),
+                                      style: const TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.w800),
+                                    ),
+                                    const SizedBox(
+                                      height: 8,
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerRight,
+                                      child: Container(
+                                        height: 40,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                            color: Colors.green,
+                                            borderRadius:
+                                                BorderRadius.circular(8)),
+                                        child: const Center(
+                                          child: Text(
+                                            "Add to Cart",
+                                            style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.white),
+                                          ),
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+          ),
         ],
       ),
     );
---
+++ lib/utils/utils.dart
@@ -0,0 +1,1 @@
+export 'db_helper.dart';
